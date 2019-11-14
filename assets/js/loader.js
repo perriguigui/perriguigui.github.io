03c26d43cfef19a03ab9361f420fbb8d8a74fa01
Perrin Antonin <tom.per@orange.fr>
2019-11-14 14:16:21 +0100

    loader

---
+++ assets/js/loader.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var elem = document.querySelector('#preloader');
+    elem.style.display = 'none';
+    elem.parentNode.removeChild(elem);
+  });
+  
